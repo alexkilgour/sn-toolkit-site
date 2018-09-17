2be6bb7a246bf10ed9d675de39c9412a2ef05830
Alex Kilgour <alexjkilgour@gmail.com>
2018-09-17 14:35:02 +0100

    better data management

--- index.js
+++ index.js
@@ -4,30 +4,16 @@ const path = require('path');
 const packageFinder = require('@springernature/util-package-finder');
 const views = require('koa-views');
 const serve = require('koa-static');
-const request = require('koa-http-request');
 const Koa = require('koa');
 const app = module.exports = new Koa();
 
-let data;
-
-// grab the latest global package data
-packageFinder({
-	deprecated: true,
-	versions: true
-})
-	.then(response => {
-		data = {
-			packages: response
-		};
-	});
-
-// app
 app.use(serve('public'));
 app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }));
 
-// render
+// grab the latest package data & render
 app.use(async function (ctx) {
-	await ctx.render('index', data);
+	const response = await packageFinder({deprecated: true, versions: true})
+	await ctx.render('index', {packages: response});
 });
 
 if (!module.parent) {
